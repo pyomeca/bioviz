6dcfa82a937ff09093f1860642b78a4810849c25
Romain Martinez <martinez.staps@gmail.com>
2018-12-18 16:32:43 -0500

    update repo

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
+import yaml
 from setuptools import setup
 
-# to install dependencies in a clean conda env, run: `conda env create -f env.yml`
+import versioneer
 
-requirements = []
+with open("env.yml", 'r') as stream:
+    out = yaml.load(stream)
+    requirements = out['dependencies'][1:]  # we do not return python
 
 setup(
     name='pyoviz',
-    version='0.1.1',
-    description="Toolbox for biomechanics analysis",
-    author="Romain Martinez & Pariterre",
-    author_email='',
-    url='https://github.com/pyomeca/pyoviz',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Pyomeca visualization toolkit",
+    author="Romain Martinez",
+    author_email='martinez.staps@gmail.com',
+    url='https://github.com/pyomeca/pyomeca',
     license='Apache 2.0',
     packages=['pyoviz'],
     install_requires=requirements,
     keywords='pyoviz',
     classifiers=[
         'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: 3.7',
     ]
 )
-
-# TODO: update requirements
