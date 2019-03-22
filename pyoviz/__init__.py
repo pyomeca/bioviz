b05044bd37d8c0dc68cd938ac2ef0cd44e175f1e
pariterre <pariterre@hotmail.com>
2019-03-22 10:40:17 -0400

    Add back VTK

--- pyoviz/__init__.py
+++ pyoviz/__init__.py
@@ -1,7 +1,7 @@
 from .fileio import *
 from .mesh import *
 from .verif import *
-from .vtk import *
+# from .vtk import *  # Removed because pyqt causes problems with the language
 
 from ._version import get_versions
 
