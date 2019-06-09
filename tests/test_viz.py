3e9faebbdbdc3b4de762bd5afd907e11919dc783
Pariterre <pariterre@hotmail.com>
2019-06-09 00:28:44 -0400

    trying to make test work

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -5,9 +5,9 @@ from BiorbdViz import BiorbdViz
 def test_model_load():
     model_path = "examples/pyomecaman.s2mMod"
 
-    # # From path
-    # b1 = BiorbdViz(model_path=model_path, show_muscles=False, show_meshes=False)
-    #
-    # # From a loaded model
-    # m = biorbd.s2mMusculoSkeletalModel(model_path)
-    # b1 = BiorbdViz(loaded_model=m, show_muscles=False, show_meshes=False)
+    # From path
+    b1 = BiorbdViz(model_path=model_path, show_muscles=False, show_meshes=False)
+    
+    # From a loaded model
+    m = biorbd.s2mMusculoSkeletalModel(model_path)
+    b1 = BiorbdViz(loaded_model=m, show_muscles=False, show_meshes=False)
